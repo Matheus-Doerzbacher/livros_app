f18870c5b87931dee2f290765dfea715b93aa08e
Matheus Doerzbacher <matheus.doer@gmail.com>
2024-09-04 11:56:54 -0300

    criado os contratos de repositories para: create_livro, get_all_livros, get_by_id_livro

--- lib/layers/domain/usecases/livro_usecase/get_all_livros/get_all_livros_usecase_imp.dart
+++ lib/layers/domain/usecases/livro_usecase/get_all_livros/get_all_livros_usecase_imp.dart
@@ -1,11 +1,13 @@
 import 'package:livros_app/layers/domain/entities/livro_entity.dart';
+import 'package:livros_app/layers/domain/repositories/livro_repositories/get_all_livros_repository.dart';
 import 'package:livros_app/layers/domain/usecases/livro_usecase/get_all_livros/get_all_livros_usecase.dart';
 
 class GetAllLivrosUsecaseImp implements GetAllLivrosUsecase {
+  final GetAllLivrosRepository _getAllLivrosRepository;
+
+  GetAllLivrosUsecaseImp(this._getAllLivrosRepository);
   @override
-  List<Livro> call() {
-    // Aqui você deve implementar a lógica para obter todos os livros
-    // Por enquanto, retornaremos uma lista vazia
-    return [];
+  Future<List<Livro>> call() async {
+    return _getAllLivrosRepository();
   }
 }
--- lib/layers/domain/usecases/livro_usecase/get_by_id_livro/get_by_id_livro_usecases_imp.dart
+++ lib/layers/domain/usecases/livro_usecase/get_by_id_livro/get_by_id_livro_usecases_imp.dart
@@ -1,24 +1,29 @@
 import 'package:livros_app/layers/domain/entities/livro_entity.dart';
+import 'package:livros_app/layers/domain/repositories/livro_repositories/get_by_id_livro_repository.dart';
 import 'package:livros_app/layers/domain/usecases/livro_usecase/get_by_id_livro/get_by_id_livro_usecases.dart';
 
 class GetByIdLivroUsecasesImp implements GetByIdLivroUsecases {
+  final GetByIdLivroRepository _getByIdLivroRepository;
+
+  GetByIdLivroUsecasesImp(this._getByIdLivroRepository);
+
   @override
-  Livro call(String id) {
-    // Por enquanto, retornaremos um livro fictício
-    // Em uma implementação real, buscaríamos o livro pelo ID
-    return Livro(
-      id: id,
-      titulo: 'Livro Exemplo',
-      autor: 'Autor Desconhecido',
-      paginas: 200,
-      ano: 2023,
-      estoque: 10,
-      avaliacao: 4,
-      editora: 'Editora Exemplo',
-      categorias: [],
-      descricao: 'Uma descrição exemplo do livro.',
-      status: StatusLeitura.queroLer,
-      urlImage: 'https://exemplo.com/imagem.jpg',
-    );
+  Future<Livro> call(String idLivro) async {
+    return await _getByIdLivroRepository(idLivro);
   }
 }
+
+    // return Livro(
+    //   id: id,
+    //   titulo: 'Livro Exemplo',
+    //   autor: 'Autor Desconhecido',
+    //   paginas: 200,
+    //   ano: 2023,
+    //   estoque: 10,
+    //   avaliacao: 4,
+    //   editora: 'Editora Exemplo',
+    //   categorias: [],
+    //   descricao: 'Uma descrição exemplo do livro.',
+    //   status: StatusLeitura.queroLer,
+    //   urlImage: 'https://exemplo.com/imagem.jpg',
+    // );
