3ef2fad0fd31fd03a387a7cbd738dabe4bacfd1a
Matheus Doerzbacher <matheus.doer@gmail.com>
2024-09-04 16:08:58 -0300

    ajustada a implementacao dos usecases de categorias

--- lib/layers/domain/usecases/categoria_usecase/get_categorias/get_categorias_usecase_imp.dart
+++ lib/layers/domain/usecases/categoria_usecase/get_categorias/get_categorias_usecase_imp.dart
@@ -1,9 +1,14 @@
 import 'package:livros_app/layers/domain/entities/categoria_entity.dart';
+import 'package:livros_app/layers/domain/repositories/categoria_repositories/get_categorias_repository.dart';
 import 'package:livros_app/layers/domain/usecases/categoria_usecase/get_categorias/get_categorias_usecase.dart';
 
 class GetCategoriasUsecaseImp implements GetCategoriasUsecase {
+  final GetCategoriasRepository _getCategoriasRepository;
+
+  GetCategoriasUsecaseImp(this._getCategoriasRepository);
+
   @override
   Future<List<Categoria>> call() async {
-    return [];
+    return await _getCategoriasRepository();
   }
 }
--- lib/layers/domain/usecases/categoria_usecase/update_categoria/update_categoria_usecase_imp.dart
+++ lib/layers/domain/usecases/categoria_usecase/update_categoria/update_categoria_usecase_imp.dart
@@ -1,9 +1,13 @@
 import 'package:livros_app/layers/domain/entities/categoria_entity.dart';
+import 'package:livros_app/layers/domain/repositories/categoria_repositories/update_categoria_repository.dart';
 import 'package:livros_app/layers/domain/usecases/categoria_usecase/update_categoria/update_categoria_usecase.dart';
 
 class UpdateCategoriaUsecaseImp implements UpdateCategoriaUsecase {
+  final UpdateCategoriaRepository _updateCategoriaRepository;
+
+  UpdateCategoriaUsecaseImp(this._updateCategoriaRepository);
   @override
   Future<bool> call(Categoria categoria) async {
-    return false;
+    return await _updateCategoriaRepository(categoria);
   }
 }
