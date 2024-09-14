7c89dc71e3542623808bad1d0a5a3051692ee564
Matheus Doerzbacher <matheus.doer@gmail.com>
2024-09-14 10:38:23 -0400

    criado o controller para emprestimo

---
+++ lib/layers/presentation/controllers/emprestimo_controller.dart
@@ -0,0 +1,42 @@
+import 'package:livros_app/layers/domain/entities/emprestimo_entity.dart';
+import 'package:livros_app/layers/domain/usecases/emprestimo_usecase/emprestimo_usecase.dart';
+
+class EmprestimoController {
+  final CreateEmprestimoUsecase _createEmprestimoUsecase;
+  final GetEmprestimoByIdUsecase _getEmprestimoByIdUsecase;
+  final GetEmprestimosUsecase _getEmprestimosUsecase;
+  final UpdateEmprestimoUsecase _updateEmprestimoUsecase;
+  final DeleteEmprestimoUsecase _deleteEmprestimoUsecase;
+
+  EmprestimoController(
+    this._createEmprestimoUsecase,
+    this._getEmprestimoByIdUsecase,
+    this._getEmprestimosUsecase,
+    this._updateEmprestimoUsecase,
+    this._deleteEmprestimoUsecase,
+  ) {
+    getEmprestimos();
+  }
+
+  late List<Emprestimo> emprestimos;
+
+  getEmprestimoById(String idLivro) async {
+    return await _getEmprestimoByIdUsecase(idLivro);
+  }
+
+  getEmprestimos() async {
+    emprestimos = await _getEmprestimosUsecase();
+  }
+
+  createEmprestimo(Emprestimo emprestimo) async {
+    return await _createEmprestimoUsecase(emprestimo);
+  }
+
+  updateEmprestimo(Emprestimo emprestimo) async {
+    return await _updateEmprestimoUsecase(emprestimo);
+  }
+
+  deleteEmprestimo(String idEmprestimo) async {
+    return await _deleteEmprestimoUsecase(idEmprestimo);
+  }
+}
