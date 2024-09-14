f76581f8894285610373ed45e17fd449d6523b51
Matheus Doerzbacher <matheus.doer@gmail.com>
2024-09-13 21:24:40 -0300

    criada as clases para livro_update

---
+++ lib/layers/data/datasources/livro_datasources/firebase/update_livro_firebase_datasource_imp.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:livros_app/layers/data/datasources/livro_datasources/update_livro_datasource.dart';
+import 'package:livros_app/layers/domain/entities/livro_entity.dart';
+
+class UpdateLivroFirebaseDatasourceImp implements UpdateLivroDatasource {
+  @override
+  Future<bool> call(Livro livro) async {
+    try {
+      await FirebaseFirestore.instance
+          .collection('livros')
+          .doc(livro.id)
+          .update(livro.toJson());
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+}
