2b2c6de37281adb7edcfddebcb4ce34d714c8202
Matheus-NUC <matheus.doer@gmail.com>
2024-09-16 11:43:10 -0300

    ajustado o usuario_entity

--- lib/layers/domain/entities/usuario_entity.dart
+++ lib/layers/domain/entities/usuario_entity.dart
@@ -1,16 +1,16 @@
 class Usuario {
   final String nome;
   final String email;
+  final String urlImage;
 
-  const Usuario({
-    required this.nome,
-    required this.email,
-  });
+  const Usuario(
+      {required this.nome, required this.email, required this.urlImage});
 
   factory Usuario.fromJson(Map<String, dynamic> json) {
     return Usuario(
       nome: json['nome'] as String,
       email: json['email'] as String,
+      urlImage: json['urlImage'] as String,
     );
   }
 
@@ -18,6 +18,7 @@ class Usuario {
     return {
       'nome': nome,
       'email': email,
+      'urlImage': urlImage,
     };
   }
 }
